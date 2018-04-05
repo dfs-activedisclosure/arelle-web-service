eb8894e704f6c071d94e4c6c20a618ad06355fbd
James White <james.white@dfsco.com>
2018-04-05 08:06:35 -0700

    Remove unnecessary taxonomies

--- docker-setup.sh
+++ docker-setup.sh
@@ -5,8 +5,5 @@
 # localhost:8080/rest/configure?packages=show
 # localhost:8080/rest/xbrl/validation?file=/taxonomies/vincent-ruane-builders-ltd-current-year-xbrl.html&media=json
 
-python3 arelleCmdLine.py --packages +UK-ALL-2009-09-01-package.zip
-python3 arelleCmdLine.py --packages +ie-all-2012-12-01.zip
-
 # run the server
 python3 arelleCmdLine.py --webserver 0.0.0.0:8080
